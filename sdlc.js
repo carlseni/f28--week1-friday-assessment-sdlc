f87288cc4c9296d2ede8f19dbaa5ee8053e9b6d0
Ian Carlsen <ianwcarlsen@gmail.com>
2022-08-19 12:09:21 -0600

    1st commit of sdlc questions

---
+++ sdlc.js
@@ -0,0 +1,39 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+
+// Define each process of the life cycle of software development then add a test output by adding a message to display when code finishes running.
+
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const AnalysisofRequirements = "What do I need to make this happen from the planning phase. What type of computer and software do I need to run this, How many people on my team do I need to develp this, etc."
+
+const design = "Graphicly what do I want this to look like for the user of my project, how do I want my code too look like and how am I going to organize the code."
+
+const Implementation = "Get to coding... Write the project code meet the needs of planning and analysis of requirements."
+
+const testingandintergration = "run the code to test if it works as intented(initial bug finding) and making sure it works on the platforms you are intending"
+
+const maintain = "You want to include more features, updates, and make sure it is meeting current needs."
+
+console.log('test problems answered')
