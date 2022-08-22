cca8754e2986bf8a75dbe5847cfa56eaa8511505
Ian Carlsen <ianwcarlsen@gmail.com>
2022-08-22 01:25:14 -0600

    third commit

--- variables.js
+++ variables.js
@@ -2,16 +2,22 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+var myName = "Ian"
+ console.log(myName)
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+var faveNum = 7
+console.log(faveNum)
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+// make lovesCode to be true or false statement the output answer
+let lovesCode = true
+console.log (lovesCode)
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +27,11 @@
 */
 
 //CODE HERE
+if (faveNum === 13){
+  console.log("lucky 13!")
+} else{
+  console.log('not lucky 13')
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +39,9 @@
 */
 
 //CODE HERE
+for (let i= 0; i<5; i++){
+console.log (faveNum)
+}
+
+
+console.log ('variables test')
